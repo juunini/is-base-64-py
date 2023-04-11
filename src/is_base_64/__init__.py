b232c556c20a7cf0d55240b00cd693ebcc903dda
juunini <juuni.ni.i@gmail.com>
2023-04-11 21:46:02 +0900

    fix: add module import in __init__ file

--- src/is_base_64/__init__.py
+++ src/is_base_64/__init__.py
@@ -1,1 +1,2 @@
-
+"""is_base_64 package."""
+from .is_base_64 import *
--- src/is_base_64/is_base_64.py
+++ src/is_base_64/is_base_64.py
@@ -1,9 +1,12 @@
+"""is_base_64 package."""
 from base64 import b64decode
 
 
 def is_base_64(value: str) -> bool:
+    """Check if a string is base64 encoded."""
     try:
         b64decode(value, validate=True)
         return True
+    # pylint: disable=bare-except
     except:
         return False
